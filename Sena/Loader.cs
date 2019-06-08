7fb005881b4ca5d3f0f18f488317b39dd681a5b2
JiongSena <51418112+JiongSena@users.noreply.github.com>
2019-06-09 03:24:56 +0800

    Update Loader.cs

--- Sena/Loader.cs
+++ Sena/Loader.cs
@@ -11,7 +11,7 @@ namespace Sena
             //RoR2.RoR2Application.isModded = true;
             while (BaseObject = UnityEngine.GameObject.Find("Sena"))
                 UnityEngine.GameObject.Destroy(BaseObject);
-            BaseObject = new UnityEngine.GameObject("Risk Of Shame");
+            BaseObject = new UnityEngine.GameObject("Sena");
             UnityEngine.Object.DontDestroyOnLoad(BaseObject);
             BaseObject.SetActive(false);
             var types = Assembly.GetExecutingAssembly().GetTypes().ToList().Where(t => t.BaseType == typeof(UnityEngine.MonoBehaviour) && !t.IsNested);
