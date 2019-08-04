c59c76399b0dcee7c6f3176ca3f4a6bb45fff549
JiongSena <51418112+JiongSena@users.noreply.github.com>
2019-08-04 14:21:27 +0800

    Update Testing2.cs

--- Sena/Testing2.cs
+++ Sena/Testing2.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 namespace Sena
 {
-    public class 25%攻速 : MonoBehaviour
+    public class 超级属性2 : MonoBehaviour
     {
         T CopyComponent<T>(T original, GameObject destination) where T : Component
         {
@@ -125,8 +125,9 @@ namespace Sena
             RoR2.RuleCatalog.allCategoryDefs.FirstOrDefault(c => c.displayToken == "RULE_HEADER_ITEMS").hiddenTest = new Func<bool>(False);
             RoR2.RuleCatalog.allCategoryDefs.FirstOrDefault(c => c.displayToken == "RULE_HEADER_EQUIPMENT").hiddenTest = new Func<bool>(False);
             RoR2.RuleCatalog.allCategoryDefs.FirstOrDefault(c => c.displayToken == "RULE_HEADER_MISC").hiddenTest = new Func<bool>(False);*/
-            RoR2.LocalUserManager.GetFirstLocalUser().cachedBody.baseAttackSpeed = 2.5f;
 
+            RoR2.LocalUserManager.GetFirstLocalUser().cachedBody.baseJumpCount = 100;
+            RoR2.LocalUserManager.GetFirstLocalUser().cachedBody.baseAttackSpeed = 100;
             /*var sb = new StringBuilder();
             var chests = UnityEngine.Object.FindObjectsOfType<RoR2.ChestBehavior>();
             foreach (var obj in chests)
