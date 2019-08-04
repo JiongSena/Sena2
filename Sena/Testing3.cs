064b6ce425d3d7a7e5377e416bd70cac4f162013
JiongSena <51418112+JiongSena@users.noreply.github.com>
2019-08-04 14:24:33 +0800

    Update and rename Testing3 to Testing3.cs

--- Sena/Testing3.cs
+++ Sena/Testing3.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 namespace Sena
 {
-    public class 超级属性2 : MonoBehaviour
+    public class 25%攻速 : MonoBehaviour
     {
         T CopyComponent<T>(T original, GameObject destination) where T : Component
         {
@@ -126,12 +126,7 @@ namespace Sena
             RoR2.RuleCatalog.allCategoryDefs.FirstOrDefault(c => c.displayToken == "RULE_HEADER_EQUIPMENT").hiddenTest = new Func<bool>(False);
             RoR2.RuleCatalog.allCategoryDefs.FirstOrDefault(c => c.displayToken == "RULE_HEADER_MISC").hiddenTest = new Func<bool>(False);*/
 
-            RoR2.LocalUserManager.GetFirstLocalUser().cachedMasterController.master.money = 10000000;
-            RoR2.LocalUserManager.GetFirstLocalUser().cachedBody.baseMoveSpeed = 20.0f;
-            RoR2.LocalUserManager.GetFirstLocalUser().cachedBody.baseJumpCount = 100;
-            RoR2.LocalUserManager.GetFirstLocalUser().cachedBody.baseAttackSpeed = 100;
-            RoR2.LocalUserManager.GetFirstLocalUser().cachedBody.healthComponent.godMode= true;
-            RoR2.LocalUserManager.GetFirstLocalUser().cachedBody.baseDamage = 10000000;
+            RoR2.LocalUserManager.GetFirstLocalUser().cachedBody.baseAttackSpeed = 2.5f;
 
             /*var sb = new StringBuilder();
             var chests = UnityEngine.Object.FindObjectsOfType<RoR2.ChestBehavior>();
